056a7577ff1aada399f0dd0c791a44dc037b5c35
sanjeev <sanjeev.test.com>
2018-06-05 09:48:03 -0600

    ASP.NET Core MVC with REST endpoints Lab

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -25,16 +25,18 @@ namespace PalTracker
         {
             services.AddMvc();
 
-           services.AddSingleton(sp => new WelcomeMessage(
-               Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")
-           ));
+            services.AddSingleton(sp => new WelcomeMessage(
+                Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")
+            ));
 
-           services.AddSingleton(cfi => new CloudFoundryInfo(
-               Configuration.GetValue<string>("PORT","PORT not configured."),
-                Configuration.GetValue<string>("MEMORY_LIMIT","MEMORY_LIMIT not configured."),
-               Configuration.GetValue<string>("CF_INSTANCE_INDEX","CF_INSTANCE_INDEX not configured."),
-               Configuration.GetValue<string>("CF_INSTANCE_ADDR","CF_INSTANCE_ADDR not configured.")
-           ));
+            services.AddSingleton(sp => new CloudFoundryInfo(
+                Configuration.GetValue<string>("PORT"),
+                Configuration.GetValue<string>("MEMORY_LIMIT"),
+                Configuration.GetValue<string>("CF_INSTANCE_INDEX"),
+                Configuration.GetValue<string>("CF_INSTANCE_ADDR")
+            ));
+
+            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
